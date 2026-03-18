1c5ce988161a1365e2b07049018fcb148472bcc6
raffa <rem_1706@proton.me>
2026-03-18 16:11:19 +0100

    [Update] Word

--- Consoles/25jInsertMaierHebeisen.sql
+++ Consoles/25jInsertMaierHebeisen.sql
@@ -1,11 +1,9 @@
 USE Library;
 
-
-
 -- ------------------------------------------------------------
 -- Import: Genre
 -- ------------------------------------------------------------
-LOAD DATA LOCAL INFILE '/Volumes/T7/Arbeit-Schule/Gibb/Unterricht Engler/Modul 164/Database/Projektarbeit-M164/CSV/Genre.csv'
+LOAD DATA LOCAL INFILE 'C:\\Lehre\\25.25-Gibb\\M164\\Projektarbeit-M164\\CSV\\Genre.csv'
 INTO TABLE Genre
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
@@ -17,7 +15,7 @@ IGNORE 1 ROWS
 -- ------------------------------------------------------------
 -- Import: Benutzer
 -- ------------------------------------------------------------
-LOAD DATA LOCAL INFILE '/Volumes/T7/Arbeit-Schule/Gibb/Unterricht Engler/Modul 164/Database/Projektarbeit-M164/CSV/Benutzer.csv'
+LOAD DATA LOCAL INFILE 'C:\\Lehre\\25.25-Gibb\\M164\\Projektarbeit-M164\\CSV\\Benutzer.csv'
 INTO TABLE Benutzer
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
@@ -29,7 +27,7 @@ IGNORE 1 ROWS
 -- ------------------------------------------------------------
 -- Import: Film
 -- ------------------------------------------------------------
-LOAD DATA LOCAL INFILE '/Volumes/T7/Arbeit-Schule/Gibb/Unterricht Engler/Modul 164/Database/Projektarbeit-M164/CSV/Film.csv'
+LOAD DATA LOCAL INFILE 'C:\\Lehre\\25.25-Gibb\\M164\\Projektarbeit-M164\\CSV\\Film.csv'
 INTO TABLE Film
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
@@ -41,7 +39,7 @@ IGNORE 1 ROWS
 -- ------------------------------------------------------------
 -- Import: Serie
 -- ------------------------------------------------------------
-LOAD DATA LOCAL INFILE '/Volumes/T7/Arbeit-Schule/Gibb/Unterricht Engler/Modul 164/Database/Projektarbeit-M164/CSV/Serie.csv'
+LOAD DATA LOCAL INFILE 'C:\\Lehre\\25.25-Gibb\\M164\\Projektarbeit-M164\\CSV\\Serie.csv'
 INTO TABLE Serie
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
@@ -53,7 +51,7 @@ IGNORE 1 ROWS
 -- ------------------------------------------------------------
 -- Import: Episode
 -- ------------------------------------------------------------
-LOAD DATA LOCAL INFILE '/Volumes/T7/Arbeit-Schule/Gibb/Unterricht Engler/Modul 164/Database/Projektarbeit-M164/CSV/Episode.csv'
+LOAD DATA LOCAL INFILE 'C:\\Lehre\\25.25-Gibb\\M164\\Projektarbeit-M164\\CSV\\Episode.csv'
 INTO TABLE Episode
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
@@ -65,7 +63,7 @@ IGNORE 1 ROWS
 -- ------------------------------------------------------------
 -- Import: FilmGenre
 -- ------------------------------------------------------------
-LOAD DATA LOCAL INFILE '/Volumes/T7/Arbeit-Schule/Gibb/Unterricht Engler/Modul 164/Database/Projektarbeit-M164/CSV/FilmGenre.csv'
+LOAD DATA LOCAL INFILE 'C:\\Lehre\\25.25-Gibb\\M164\\Projektarbeit-M164\\CSV\\FilmGenre.csv'
 INTO TABLE FilmGenre
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
@@ -77,7 +75,7 @@ IGNORE 1 ROWS
 -- ------------------------------------------------------------
 -- Import: SerieGenre
 -- ------------------------------------------------------------
-LOAD DATA LOCAL INFILE '/Volumes/T7/Arbeit-Schule/Gibb/Unterricht Engler/Modul 164/Database/Projektarbeit-M164/CSV/SerieGenre.csv'
+LOAD DATA LOCAL INFILE 'C:\\Lehre\\25.25-Gibb\\M164\\Projektarbeit-M164\\CSV\\SerieGenre.csv'
 INTO TABLE SerieGenre
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
@@ -89,7 +87,7 @@ IGNORE 1 ROWS
 -- ------------------------------------------------------------
 -- Import: Bewertung
 -- ------------------------------------------------------------
-LOAD DATA LOCAL INFILE '/Volumes/T7/Arbeit-Schule/Gibb/Unterricht Engler/Modul 164/Database/Projektarbeit-M164/CSV/Bewertung.csv'
+LOAD DATA LOCAL INFILE 'C:\\Lehre\\25.25-Gibb\\M164\\Projektarbeit-M164\\CSV\\Bewertung.csv'
 INTO TABLE Bewertung
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
@@ -101,7 +99,7 @@ IGNORE 1 ROWS
 -- ------------------------------------------------------------
 -- Import: WatchHistory
 -- ------------------------------------------------------------
-LOAD DATA LOCAL INFILE '/Volumes/T7/Arbeit-Schule/Gibb/Unterricht Engler/Modul 164/Database/Projektarbeit-M164/CSV/WatchHistory.csv'
+LOAD DATA LOCAL INFILE 'C:\\Lehre\\25.25-Gibb\\M164\\Projektarbeit-M164\\CSV\\WatchHistory.csv'
 INTO TABLE WatchHistory
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
